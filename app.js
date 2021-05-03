53b169f0010131806c50622ae6cd997ec42ec7dd
sunku prashanth <sunkuprashanth07@gmail.com>
2021-05-03 21:01:55 +0530

    initial

--- app.js
+++ app.js
@@ -148,7 +148,7 @@ app.get("/api/booking", auth, async (req, res) => {
 
 if (process.env.NODE_ENV == 'production') {
 	app.use(express.static("client/build"));
-	app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, 'build', 'index.html')))
+	app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 }
 
 const PORT = process.env.PORT || 5000;
